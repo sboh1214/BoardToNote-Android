892af34a4b62b5423a8bbc5668530ff6aeb1f787
오승빈 <sboh1214@gmail.com>
2019-05-08 11:06:37 +0900

    edit text instantly

--- Application/src/main/java/com/underpressure/boardtonote/EditActivity.kt
+++ Application/src/main/java/com/underpressure/boardtonote/EditActivity.kt
@@ -4,14 +4,12 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_edit.*
 import kotlinx.android.synthetic.main.content_edit.*
-import kotlinx.android.synthetic.main.dialog_rename_title.*
 
 
 class EditActivity : AppCompatActivity() {
@@ -53,10 +51,6 @@ class EditActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.Menu_Rename -> {
-                Rename()
-                return true
-            }
             R.id.Menu_Save -> {
                 return true
             }
@@ -67,20 +61,4 @@ class EditActivity : AppCompatActivity() {
         }
     }
 
-    private fun Rename() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Edit Title")
-        val inflater = layoutInflater
-        val dialogLayout = inflater.inflate(R.layout.dialog_rename_title, null)
-        builder.setView(dialogLayout)
-        builder.setPositiveButton("확인") { dialogInterface, i ->
-            btnClass.Title = editText.text.toString()
-        }
-        builder.setNegativeButton("취소") { dialogInterface, i ->
-
-        }
-
-        val dialog = builder.create()
-        dialog.show()
-    }
 }
