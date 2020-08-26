6a263579ef45c710ace5f9fd4ba887252883231a
Seungbin Oh <sboh1214@gmail.com>
2020-08-26 15:33:37 +0900

    add google play review api

--- app/src/main/java/com/unitech/boardtonote/settings/AboutFragment.kt
+++ app/src/main/java/com/unitech/boardtonote/settings/AboutFragment.kt
@@ -3,29 +3,41 @@ package com.unitech.boardtonote.settings
 import android.os.Build
 import android.os.Bundle
 import android.webkit.WebView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.play.core.review.ReviewManagerFactory
 import com.unitech.boardtonote.R
 
-class AboutFragment : PreferenceFragmentCompat()
-{
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?)
-    {
+class AboutFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference_about, rootKey)
 
         val info = requireActivity().packageManager.getPackageInfo(requireActivity().packageName, 0)
         val versionName: Preference? = findPreference("Preference_VersionName")
         versionName?.title = info.versionName
         val versionCode: Preference? = findPreference("Preference_VersionCode")
-        if (Build.VERSION.SDK_INT >= 28)
-        {
+        if (Build.VERSION.SDK_INT >= 28) {
             versionCode?.title = info.longVersionCode.toString()
-        }
-        else
-        {
+        } else {
             @Suppress("DEPRECATION")
             versionCode?.title = info.versionCode.toString()
+        }
+
+        val reviewButton = findPreference<Preference>("Preference_Review")
+        reviewButton!!.setOnPreferenceClickListener {
+            val manager = ReviewManagerFactory.create(it.context)
+            val request = manager.requestReviewFlow()
+            request.addOnCompleteListener { result ->
+                if (result.isSuccessful) {
+                    val reviewInfo = result.result
+                    Toast.makeText(it.context, "Success", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(it.context, "Error", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
         }
 
         val btnLicense = findPreference<Preference>("Preference_BTNLicense")
